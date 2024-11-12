8a6a69bb5d9cdd9c08af3766ff1f3e621da61b16
jodroassis <joao.santos006@sptech.school>
2024-11-12 14:41:27 -0300

    update script BD

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -9,12 +9,6 @@ comandos para mysql server
 CREATE DATABASE ativamente;
 USE ativamente;
 -- drop database ativamente;
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,14 +17,6 @@ CREATE TABLE usuario (
 	dtNasc date,
     cpf char(11),
 	senha VARCHAR(50)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
 CREATE TABLE artigo (
@@ -42,32 +28,19 @@ CREATE TABLE artigo (
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+create table pesquisa (
+	id int primary key auto_increment,
+    fkUsuario int,
+    constraint fkUsuarioPesquisa foreign key(fkUsuario)
+		references usuario(id),
+	idade int,
+    atividade_fisica varchar(45),
+    alimentacao varchar(45),
+    sono int(45),
+    saude_mental varchar(45),
+    objetivos_saude varchar(45),
+    nivel_estresse varchar(45));
+    
+    select * from pesquisa;
+    select * from usuario;
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
-
-select * from artigo;
-select * from usuario;
